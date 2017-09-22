fbcdd905fdc9632f604ad1acc61a6c16e2af01d0
Mikhail Ponomarev <ponomarev45@gmail.com>
2017-09-22 13:58:13 +0300

    simple-todo-app

---
+++ actions/index.js
@@ -0,0 +1,23 @@
+let nextToId = 0;
+
+export const addTodo = text => {
+  return {
+    type: 'ADD_TODO',
+    id: nextToId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = filter => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = id => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
